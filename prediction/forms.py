78a301f31848089b69f0ff040aef41187b7313b3
EDC SIESGST <edc@siesgst.ac.in>
2025-03-10 21:32:27 +0530

    CHNAGES

--- prediction/forms.py
+++ prediction/forms.py
@@ -1,5 +1,6 @@
-from django import forms
+from django.urls import path
+from . import views
 
-class StockPredictionForm(forms.Form):
-    stock_ticker = forms.CharField(label='Stock Ticker', max_length=20)
-    time_horizon = forms.IntegerField(label='Time Horizon (in months)', min_value=1, max_value=12)
+urlpatterns = [
+    path('prediction/', views.stock_prediction_view, name='stock_prediction'),
+]
